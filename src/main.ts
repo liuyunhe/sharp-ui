56c9a88c48258ff65ba5b01e509a7714bef77b15
liuyunhe <liuyunhe666@aliyun.com>
2024-04-28 09:33:32 +0800

    feat: 添加图标库并更新样式导入顺序

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import '@/styles/index.css'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+library.add(fas)
+import './styles/index.css'
+console.log(App.render?.toString())
 createApp(App).mount('#app')
