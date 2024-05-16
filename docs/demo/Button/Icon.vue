cf5b2a7763767eeb42db31b937a1863f9da49e79
liuyunhe <liuyunhe666@aliyun.com>
2024-05-16 09:03:55 +0800

    feat: 添加 Button 组件文档和示例

---
+++ docs/demo/Button/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="margin-bottom: 20px">
+    <Button type="primary" icon="search">搜索</Button>
+    <Button icon="edit">编辑</Button>
+    <Button type="info" icon="message">信息按钮</Button>
+    <Button type="danger" icon="trash">危险按钮</Button>
+    <Button type="success" icon="share">分享</Button>
+  </div>
+  <div>
+    <Button type="primary" icon="search" circle></Button>
+    <Button icon="edit" circle></Button>
+    <Button type="success" icon="share" circle></Button>
+  </div>
+</template>
+
+<script setup>
+import Button from '@/components/Button/Button.vue'
+</script>
---
+++ docs/demo/Button/Text.vue
@@ -0,0 +1,12 @@
+<template>
+  <Button text>文字按钮</Button>
+  <Button type="primary" text>主要按钮</Button>
+  <Button type="success" text>成功按钮</Button>
+  <Button type="info" text>信息按钮</Button>
+  <Button type="warning" text>警告按钮</Button>
+  <Button type="danger" text>危险按钮</Button>
+</template>
+
+<script setup>
+import Button from '@/components/Button/Button.vue'
+</script>
