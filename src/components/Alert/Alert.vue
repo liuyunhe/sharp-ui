2ce5bde1514abad9d1871b5f20a021f7eec75514
liuyunhe <liuyunhe666@aliyun.com>
2024-05-27 17:38:59 +0800

    feat: 添加 Alert 组件，支持自定义消息提示的样式和行为

--- src/components/Alert/Alert.vue
+++ src/components/Alert/Alert.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
+  <div role="alert" class="s-alert s-alert--info is-dark">
+    <div class="s-alert__content">
+      <span class="s-alert__title">消息提示的文案</span
+      >
 
+    </div>
+    <div class="s-alert__close">
+      <Icon icon="xmark" @click.stop="" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+  import Icon from '@/components/Icon/Icon.vue'
+  defineOptions({
+    name: 'S-Alert',
+  })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
