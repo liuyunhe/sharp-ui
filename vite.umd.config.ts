9756510ce2c83d68e490b46a09ab0c0bef6a6d43
liuyunhe <liuyunhe666@aliyun.com>
2024-06-06 09:11:54 +0800

    feat: 配置 Vite 构建工具以支持 ES 和 UMD 格式输出

---
+++ vite.umd.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist/umd',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'SharpUI',
+      fileName: 'sharp-ui',
+      formats: ['umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue'
+        },
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'style.css') {
+            return 'index.css'
+          }
+          return chunkInfo.name as string
+        }
+      }
+    }
+  }
+})
