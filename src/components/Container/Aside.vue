bd15bb1b9a1005fd854b96911a38ad21df904c7e
liuyunhe <liuyunhe666@aliyun.com>
2024-06-27 09:07:55 +0800

    feat: 添加了容器组件

---
+++ src/components/Container/Aside.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside :class="ns.b()" :style="style">
+    <slot />
+  </aside>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useNamespace } from '@/hooks/useNameSpace'
+
+import type { CSSProperties } from 'vue'
+
+defineOptions({
+  name: 'SAside',
+})
+const props = defineProps({
+  /**
+   * @description width of the side section
+   */
+  width: {
+    type: String,
+    default: null,
+  },
+})
+
+const ns = useNamespace('aside')
+const style = computed(
+  () =>
+    (props.width ? ns.cssVarBlock({ width: props.width }) : {}) as CSSProperties
+)
+</script>
---
+++ src/components/Container/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <main :class="ns.b()">
+    <slot />
+  </main>
+</template>
+<script lang="ts" setup>
+import { useNamespace } from '@/hooks/useNameSpace'
+
+defineOptions({
+  name: 'SMain',
+})
+
+const ns = useNamespace('main')
+</script>
