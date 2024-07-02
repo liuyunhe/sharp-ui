c9eb22c9d1b2ca19682753732b89ceae8712dabe
liuyunhe <liuyunhe666@aliyun.com>
2024-07-02 09:08:33 +0800

    fix(MessageBox): 修改了SMessageBox的appendTo选项警告信息

--- src/components/MessageBox/messageBox.ts
+++ src/components/MessageBox/messageBox.ts
@@ -48,7 +48,7 @@ const getAppendToElement = (props: any): HTMLElement => {
     // should fallback to default value with a warning
     if (!isElement(appendTo)) {
       debugWarn(
-        'ElMessageBox',
+        'SMessageBox',
         'the appendTo option is not an HTMLElement. Falling back to document.body.'
       )
       appendTo = document.body
