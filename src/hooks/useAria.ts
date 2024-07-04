a37989e3b5f7bd4c7dfc72b2fa8198363b9ff039
liuyunhe <liuyunhe666@aliyun.com>
2024-07-04 09:13:38 +0800

    feat: 新增 useAria,useProp和 useDeprecated 钩子函数

---
+++ src/hooks/useAria.ts
@@ -0,0 +1,24 @@
+import { pick } from 'lodash-unified'
+import { buildProps } from '@/utils'
+
+export const ariaProps = buildProps({
+  /**
+   * @description native `aria-label` attribute
+   */
+  ariaLabel: String,
+  /**
+   * @description native `aria-orientation` attribute
+   */
+  ariaOrientation: {
+    type: String,
+    values: ['horizontal', 'vertical', 'undefined']
+  },
+  /**
+   * @description native `aria-controls` attribute
+   */
+  ariaControls: String
+})
+
+export const useAriaProps = <T extends keyof typeof ariaProps>(arias: Array<T>) => {
+  return pick<typeof ariaProps, T>(ariaProps, arias)
+}
